5f17735e042a900355c4573ac0386cf9b753d6fe
Rohis Rachman <66287595+rohisrachman@users.noreply.github.com>
2021-10-12 10:22:58 +0800

    Add files via upload

---
+++ Pertemuan-1/Flw-chrt-1/Temperature.java
@@ -0,0 +1,18 @@
+package flow;
+
+
+public class Temperature {
+
+	public static void main(String[] args) {
+	
+	float temp = 40;
+	
+	
+	if (temp < 32) {
+	System.out.println("Below Freezing");
+	} else if (temp > 32) {
+	System.out.println("above freezing");
+	}
+	
+	}
+}
